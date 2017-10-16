7ca88d6620a70e7f155b3a26636c2609b952d5ec
yashwanth <yashwanthr111@gmail.com>
2017-10-16 19:03:14 +0530

    added routes of all drivers to customers map but still having bugs

---
+++ app/src/main/java/com/yashwanth/ride/CustomerOrDriverActivity.java
@@ -0,0 +1,13 @@
+package com.yashwanth.ride;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerOrDriverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_or_driver);
+    }
+}
